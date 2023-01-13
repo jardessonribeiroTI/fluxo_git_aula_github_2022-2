2fc018058a75a77e7fa80b87c54b9a7c9e82ae4f
Matheus Luís Webber <matheus.webber@hotmail.com>
2023-01-13 16:52:41 -0300

    Adicionados os getters e setters e criado o método para cadastrar novos clientes

--- Client.java
+++ Client.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
+
 public class Client {
-    String name;
-    Integer age;
-    Integer cpf;
-    String email;
+    private String name;
+    private Integer age;
+    private Integer cpf;
+    private String email;
 
     public Client(String name, Integer age, Integer cpf, String email) {
         this.name = name;
@@ -11,7 +13,53 @@ public class Client {
         this.email = email;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(Integer cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public String toString() {
         return "Client [name=" + name + ", age=" + age + ", cpf=" + cpf + ", email=" + email + "]";
     }
+
+    public Client registerClient() {
+        System.out.println("Digite o nome do cliente: ");
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();
+        System.out.println("Digite a idade do cliente: ");
+        Integer age = Integer.parseInt(s.nextLine());
+        System.out.println("Digite o CPF do cliente: ");
+        Integer cpf = Integer.parseInt(s.nextLine());
+        System.out.println("Digite o email do cliente: ");
+        String email = s.nextLine();
+        Client client = new Client(name, age, cpf, email);
+        return client;
+    }    
 }
